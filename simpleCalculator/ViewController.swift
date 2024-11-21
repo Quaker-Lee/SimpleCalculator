53905b9821a552646571d9c584626513e9b69307
Quaker-Lee <tkacl2010@gmail.com>
2024-11-21 17:07:17 +0900

    Initial Commit

---
+++ simpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simpleCalculator
+//
+//  Created by 이재건 on 11/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ simpleCalculatorTests/simpleCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  simpleCalculatorTests.swift
+//  simpleCalculatorTests
+//
+//  Created by 이재건 on 11/21/24.
+//
+
+import Testing
+@testable import simpleCalculator
+
+struct simpleCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
