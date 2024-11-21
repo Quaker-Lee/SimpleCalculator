ce6295ec837175c3a2b984d69429ca05cbc911d3
Quaker-Lee <tkacl2010@gmail.com>
2024-11-22 03:06:25 +0900

    ♻️ 코드 리팩토링
    Then 라이브러리 추가 후 코드 리팩토링

--- simpleCalculator/ViewController.swift
+++ simpleCalculator/ViewController.swift
@@ -7,39 +7,78 @@
 
 import UIKit
 import SnapKit
+import Then
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        calculator()
+        
+        configureUI()
     }
     
-    // 숫자들이 보일 Label
-    let numberLabel = UILabel()
     
-    private func calculator() {
+    
+    //숫자들이 보일 Label
+    private let numberLabel = UILabel().then {
+        $0.text = "12345"
+        $0.textColor = .white
+        $0.textAlignment = .right
+        $0.font = UIFont.boldSystemFont(ofSize: 60)
+    }
+    
+//    var btn7 = UIButton()
+//    let btn8 = UIButton()
+//    let btn9 = UIButton()
+//    
+//    let btnPlus = UIButton()
+    
+  
+    
+    private func configureUI() {
         view.backgroundColor = .black
         
-        numberLabel.text = "12345"
-        numberLabel.textColor = .white
-        numberLabel.textAlignment = .right
-        numberLabel.font = UIFont.boldSystemFont(ofSize: 60)
-        
-        [numberLabel]
-            .forEach { view.addSubview($0) }
-        
+//        [numberLabel]
+//            .forEach { view.addSubview($0) }
+        view.addSubview(numberLabel)
         numberLabel.snp.makeConstraints {
             $0.height.equalTo(100)
             $0.leading.trailing.equalToSuperview().offset(-30)
             $0.top.equalToSuperview().offset(200)
         }
         
+        //btn7 = btns(title: "7")
         
     }
     
     
     
+    
+//    //구현을 위한 함수들
+//    //StackView 구현을 위한 함수
+//    private func makeHorizontalStackView(_ views: [UIView]) -> UIStackView {
+//        let stackView = UIStackView(arrangedSubviews: views)
+//        stackView.backgroundColor = .black
+//        stackView.axis = .horizontal
+//        stackView.spacing = 10
+//        stackView.distribution = .fillEqually
+//        
+//        
+//        return stackView
+//    }
+//    
+//    //버튼 구현을 위한 함수
+//    private func btns(title: String) -> UIButton {
+//        let btn = UIButton()
+//        btn.setTitle(title, for: .normal)
+//        btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
+//        btn.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+//        btn.frame.size.height = 80
+//        btn.frame.size.width = 80
+//        btn.layer.cornerRadius = 40
+//        
+//        return btn
+//    }
 
 
 }
